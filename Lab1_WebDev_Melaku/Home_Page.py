c1afef130c814199ca1c666ef06e0c49709f7eef
eyoelmelaku20-sys <eyoel.melaku20@gmail.com>
2026-09-20 22:26:58 -0400

    Add files via upload

---
+++ Lab1_WebDev_Melaku/Home_Page.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section FAC")
+st.subheader("Eyoel Melaku")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1.Portfolio: A page about my background, skills, and projects.
+2.Quiz: A short interactive quiz to test your knowledge.
+3.
+4.
+
+""")
+
---
+++ Lab1_WebDev_Melaku/info.py
@@ -0,0 +1,76 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "I'm Eyoel Melaku. I am a Industrial Engineering student at Georgia Tech looking for internships or coops. "
+
+
+#CHANGE BELOW
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://cdn-icons-png.flaticon.com/256/561/561127.png"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/eyoel-m-9a2741380/"
+my_github_url = "https://github.com/eyoelmelaku20-sys"
+my_email_address = "emelaku6@gatech.edu"
+
+
+education_data = {
+    'Degree': 'Bachelor of Science in Industrial Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2030',
+    'GPA': 'N/A'
+}
+
+course_data = {
+    "code": ["CS 1301", "ISyE 1801", "MATH 1554", "VIP 2060"],
+    "names": ["Intro to Computing", "Intro to ISyE Principles", "Linear Algebra", "Research Management"],
+    "semester_taken": ["1st", "1st", "1st", "1st"],
+    "skills": ["Python", "Optimization principles", "Matrices and Vectors", "Research skills"],
+}
+
+experience_data = {
+    "Process Improvement Lead - Gwinnett Co-op": (["- Led a process-improvement project analyzing the food distribution line",
+                                                     "- Timed cars per station and tracked work-in-progress to identify the bottleneck",
+                                                     "- Reduced average wait times by 10 minutes and increased cars served per day by 2"], "Images/cook.jpeg"),
+    "Leader - Gwinnett Leaders of America": (["- Managed and led a team through various leadership initiatives",
+                                               "- Developed organizational and communication skills through team management"], "Images/cleaner.jpeg"),
+}
+
+projects_data = {
+    "Electrify GT": "Project Team Lead for a Georgia Tech club initiative analyzing GTPD's vehicle fleet for potential electrification.",
+    "Energy Systems Group": "Collaborated with a team to explore optimizing energy access in countries including Ethiopia, Uganda, and Sudan.",
+}
+
+programming_data = {
+    "Python": 60,
+    "Excel/VBA": 75,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Excel/VBA": "📊",
+}
+spoken_icons = {
+    "English": "🇬🇧",
+    "Amharic": "🇪🇹",
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Amharic": "Fluent",
+}
+
+leadership_data = {
+    "Lead Consultant Applicant - Epic Intentions": (["- Applying for a Lead Consultant role in GT's nonprofit consulting club"], "Images/jelly.jpeg"),
+}
+
+activity_data = {
+    "YJSP (Space Avionics Club)": ["- Applying to join the operations team"],
+    "Technology Consulting Club (TCG)": ["- Active member gaining consulting exposure"],
+}
