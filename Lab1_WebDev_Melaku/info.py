2a12e00cef2705176427acf995e67609680c163d
eyoelmelaku20-sys <eyoel.melaku20@gmail.com>
2026-09-20 23:19:57 -0400

    Changed file name to all uppercase

--- Lab1_WebDev_Melaku/info.py
+++ Lab1_WebDev_Melaku/info.py
@@ -2,7 +2,7 @@
 #This File will contain the information to be displayed in your portfolio
 
 #CHANGE BELOW
-profile_picture = "Images/profile.jpeg"
+profile_picture = "Images/profile.JPEG"
 about_me = "I'm Eyoel Melaku. I am a Industrial Engineering student at Georgia Tech looking for internships or coops. "
 
 
